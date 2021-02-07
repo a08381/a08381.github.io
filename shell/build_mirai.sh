e9dc46ed2d4ab0164269dcdee054697066920793
a08381 <632785425@163.com>
2021-02-07 19:41:04 +0800

    Update build_mirai.sh

--- shell/build_mirai.sh
+++ shell/build_mirai.sh
@@ -13,14 +13,16 @@ function build_mirai() {
     HTTP_PROXY=http://192.168.0.2:8080 HTTPS_PROXY=http://192.168.0.2:8080 git clone https://github.com/mamoe/mirai
     cd mirai
     curl https://a08381.github.io/patches/01_build.patch | git apply -
-    if [ -e "mirai_files/mirai-core-all-${MIRAI_STABLE_VER}-without-bcprov.jar" ]; then
+    if [ -e "mirai_files/stable/mirai-core-all-${MIRAI_STABLE_VER}-without-bcprov.jar" ]; then
         git checkout $MIRAI_STABLE_VER
         chmod +x gradlew
         ./gradlew shadowJar
     fi
+    if [ -e "mirai_files/dev/mirai-core-all-${MIRAI_DEV_VER}-without-bcprov.jar" ]; then
         git checkout $MIRAI_DEV_VER
         chmod +x gradlew
         ./gradlew shadowJar
+    fi
     cd mirai-core-all/build/libs
     ls *.jar | sed -r 's#mirai-core-all-(.*)-all.jar#mv & mirai-core-all-\1-without-bcprov.jar#' | bash
     # rename 's/-all./-without-bcprov./' *.jar
