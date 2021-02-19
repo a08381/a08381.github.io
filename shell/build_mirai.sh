ea4e5fad63ce6ea941b65577ab0ad8ec1380037e
a08381 <632785425@163.com>
2021-02-19 16:52:52 +0800

    Update build_mirai.sh

--- shell/build_mirai.sh
+++ shell/build_mirai.sh
@@ -10,19 +10,15 @@ function get_mirai_version() {
 
 function build_mirai() {
     rm -rf mirai
-    HTTP_PROXY=http://192.168.0.2:8080 HTTPS_PROXY=http://192.168.0.2:8080 git clone https://github.com/mamoe/mirai
+    git clone https://github.com.cnpmjs.org/mamoe/mirai
     cd mirai
     curl https://a08381.github.io/patches/01_build.patch | git apply -
-    if [ ! -e "mirai_files/stable/mirai-core-all-${MIRAI_STABLE_VER}-without-bcprov.jar" ]; then
-        git checkout $MIRAI_STABLE_VER
-        chmod +x gradlew
-        ./gradlew shadowJar
-    fi
-    if [ ! -e "mirai_files/dev/mirai-core-all-${MIRAI_DEV_VER}-without-bcprov.jar" ]; then
-        git checkout $MIRAI_DEV_VER
-        chmod +x gradlew
-        ./gradlew shadowJar
-    fi
+    git checkout $MIRAI_STABLE_VER
+    chmod +x gradlew
+    ./gradlew shadowJar
+    git checkout $MIRAI_DEV_VER
+    chmod +x gradlew
+    ./gradlew shadowJar
     if [ -d "mirai-core-all/build/libs" ]; then
         cd mirai-core-all/build/libs
         ls *.jar | sed -r 's#mirai-core-all-(.*)-all.jar#mv & mirai-core-all-\1-without-bcprov.jar#' | bash
@@ -48,4 +44,4 @@ function copy_file() {
 
 get_mirai_version
 build_mirai
-copy_file
+# copy_file
