2d27dbab19d09a7e4a42cef8d37a21b35646b474
a08381 <632785425@163.com>
2021-04-20 18:41:05 +0800

    Create build_mirai_latest.sh

---
+++ shell/build_mirai_latest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MIRAI_STABLE_VER=""
+
+function get_mirai_version() {
+    MIRAI_STABLE_VER=$(git tag | grep "^v[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+$" | sed -n '$p')
+}
+
+function build_mirai() {
+    curl https://a08381.github.io/patches/01_build.patch | git apply -
+    git stash
+    git checkout $MIRAI_STABLE_VER
+    git stash apply
+    chmod +x gradlew
+    ./gradlew shadowJar
+    git checkout .
+    if [ -d "mirai-core-all/build/libs" ]; then
+        cd mirai-core-all/build/libs
+        ls *.jar | sed -r 's#mirai-core-all-(.*)-all.jar#mv & mirai-core-all-\1-without-bcprov.jar#' | bash
+    fi
+}
+
+get_mirai_version
+build_mirai
